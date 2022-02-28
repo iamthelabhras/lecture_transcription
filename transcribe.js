9c4ddd84793509f6b5d4475e53d973ffb5da4930
Labhrás Mac Gabhann <coding@larrymg.me>
2022-02-27 23:50:37 -0500

    style: Removed extraneous & duplicative commented-out code.

--- transcribe.js
+++ transcribe.js
@@ -18,7 +18,6 @@ const client = new speech.SpeechClient();
 
 // change gcsUri to whatever file I want to currently transcribe
 const gcsUri = "gs://lecture_audio/file_name_goes_here.mp3";
-//  const encoding = 'M4A';
 const encoding = "MP3";
 const sampleRateHertz = 16000;
 const languageCode = "en-US";
